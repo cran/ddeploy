00e460b7d8a36d645cf1f3f91cf55efb2269446a
Niall McGearailt <niall@dukeanalytics.com>
2015-05-04 00:00:00 +0000

    version 1.0.2

---
+++ R/duke_list.R
@@ -0,0 +1,25 @@
+#' List deployed models
+#'
+#' @param auth_details authorization parameters (must be stored in list format: list(user_name='exampleusername',api_key='exampleapikey')).
+#'
+#' @return None
+#' @export
+#' @examples
+#' 
+#' duke_auth <- list(
+#' user_name = "try_it",
+#' api_key   = "db1542b66f16aba5768d8a19c27dec4facf9168a",
+#' endpoint = "/api/v1.0"
+#' )
+#' duke_list(auth_details=duke_auth)
+
+duke_list <- function(auth_details){
+  if (!("user_name" %in% names(auth_details))) return("No user_name supplied as part of the auth_details parameter.")
+  if (!("api_key" %in% names(auth_details))) return("No api_key supplied as part of the auth_details parameter.")
+  if (!("endpoint" %in% names(auth_details))) return("No endpoint supplied as part of the auth_details parameter.")
+  r <- httr::GET(url= paste("http://deploy.dukeanalytics.com",auth_details$endpoint,"/list/",auth_details$user_name,"/",auth_details$api_key,sep=""), 
+           query = list(Ext=".rds"))
+  result <- httr::content(r,"parsed","application/json")
+  result_df <- data.frame(matrix(unlist(result), ncol=3, byrow=T));result_df<- result_df[,c(2,3,1)];names(result_df) <- c("model_name","model_size","last_update")
+  return(result_df)
+}
